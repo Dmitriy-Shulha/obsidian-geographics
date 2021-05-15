2558331a8f2bbe4727ff273aa8315e16195ae31d
Dmitriy Shulga <dimashulga1115@gmail.com>
2021-05-15 20:33:41 +0300

    Implemented tooltip.

--- src/ui/component/Map.svelte
+++ src/ui/component/Map.svelte
@@ -1,74 +1,98 @@
- <script>
+<script>
+  import { geoPath } from "d3-geo";
+  import { feature } from "topojson";
+  import { maps, COUNTRIES_110M } from "../../data/maps.js";
+  import { projections } from "../../data/projections.js";
+  import { spring } from "svelte/motion";
 
-    import { geoPath } from "d3-geo";
-    import { feature } from "topojson";
-    import { maps, COUNTRIES_110M } from "../../data/maps.js"
-    import { projections } from "../../data/projections.js"
-  
-    const width = "960";
-    const height = "500";
+  const width = "960";
+  const height = "500";
 
-    let data = [];
-    let mapName = COUNTRIES_110M;
-    let mapObjects = {};
-    let mapObjectNames = [];
-    let mapObjectName = Object.keys(maps[mapName].objects)[0];
+  let data = [];
+  let mapName = COUNTRIES_110M;
+  let mapObjects = {};
+  let mapObjectNames = [];
+  let mapObjectName = Object.keys(maps[mapName].objects)[0];
 
-    $: { 
-        console.log(mapName)
-        console.log(data)
+  $: {
+    mapObjects = maps[mapName].objects;
+    mapObjectNames = Object.keys(mapObjects);
+    mapObjectName = mapObjectNames.includes(mapObjectName)
+      ? mapObjectName
+      : mapObjectNames[0];
+    data = feature(maps[mapName], mapObjects[mapObjectName]).features;
+  }
 
-        mapObjects = maps[mapName].objects
-        mapObjectNames = Object.keys(mapObjects);
-        mapObjectName = mapObjectNames.includes(mapObjectName) 
-          ? mapObjectName 
-          : mapObjectNames[0]; 
-        data = feature(maps[mapName], mapObjects[mapObjectName]).features
-        console.log(data)
-    }
+  let currentProj = null;
+  let path = null;
+  let projectionName = "geoAlbers";
+  $: {
+    currentProj = projections[projectionName]();
+    path = geoPath().projection(currentProj);
+  }
 
-    let currentProj = null;
-    let path = null;
-    let projectionName = "geoAlbers";
-    $: {
-      currentProj = projections[projectionName]();
-      path = geoPath().projection(currentProj);
-    }
+  let currentAreaName;
+  let coords = spring({ x: 0, y: 0 });
+</script>
 
-  </script>
+<select bind:value={mapName}>
+  {#each Object.keys(maps) as name}
+    <option>{name}</option>
+  {/each}
+</select>
 
-  <select bind:value={mapName}>
-      {#each Object.keys(maps) as name }
-        <option>{name}</option>
-      {/each}
-  </select>
-  
-  <select bind:value={mapObjectName}>
-    {#each mapObjectNames as name }
-      <option>{name}</option>
-    {/each}
-  </select>
+<select bind:value={mapObjectName}>
+  {#each mapObjectNames as name}
+    <option>{name}</option>
+  {/each}
+</select>
 
-  <select bind:value={projectionName}>
-    {#each Object.keys(projections) as name }
-      <option>{name}</option>
-    {/each}
-  </select>
+<select bind:value={projectionName}>
+  {#each Object.keys(projections) as name}
+    <option>{name}</option>
+  {/each}
+</select>
 
-  <svg width="960" height="500">
+<svg width="100%" height="100%">
+  <g>
     {#each data as feature}
-    <path 
-      d={path(feature)} 
-      class="border"
-    />
+      <path
+        d={path(feature)}
+        class="border"
+        on:mouseenter={() => {}}
+        on:mousemove={(e) => {
+          coords.set({ x: e.layerX, y: e.layerY });
+          currentAreaName = feature.properties.name;
+        }}
+        on:mouseout={() => {
+          currentAreaName = undefined;
+        }}
+      />
     {/each}
-  </svg>
-  
-  <style>
-    .border {
-      stroke: #444444;
-      fill: #cccccc;
-      border: 10px solid red;
-    }
-  </style>
-  
+  </g>
+
+  <g
+    id="tooltip"
+    opacity={currentAreaName ? 70 : 0}
+    transform={`translate(${$coords.x},${$coords.y})`}
+  >
+    <rect width={`${currentAreaName?.length * 0.65 ?? 10}em`} height="3em" fill="white" />
+    <text transform="translate(5,25)">
+      {currentAreaName}
+    </text>
+  </g>
+</svg>
+
+<style>
+  .border {
+    stroke: #444444;
+    fill: #cccccc;
+    border: 10px solid red;
+  }
+
+  #tooltip {
+    /* opacity: 50%; */
+    pointer-events: none;
+    background-color: whitesmoke;
+  }
+</style>
